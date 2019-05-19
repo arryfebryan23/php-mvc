7af4d9abd17e25c9d5bcf83e32545e59f01f14f0
arryfebryan23 <arryfebryan23@gmail.com>
2019-05-19 12:45:24 +0700

    menambahkan controller

---
+++ app/controllers/About.php
@@ -0,0 +1,18 @@
+<?php 
+
+Class About{
+
+	public function index($nama = "Arry", $pekerjaan = "Mahasiswa"){
+		echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+	}
+
+	public function page()
+	{
+		echo 'About/page';
+	}
+
+
+
+}
+
+ ?>
