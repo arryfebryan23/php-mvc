ac3746f4e2a27fb88612ccf7c83c7389ad5a5ac1
arryfebryan23 <arryfebryan23@gmail.com>
2019-05-20 14:48:53 +0700

    menambahkan assets v2

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,6 @@
-
-	<h2>About Me</h2>
+	
+	<div class="container">
+	<h2 class="mt-4">About Me</h2>
+	<img src="<?= BASEURL; ?>/img/profile.jpg" alt="Arry Febryan" width="150" class="rounded-circle shadow m-1">
 	<p>Halo nama saya <?= $data['nama']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+	</div>
